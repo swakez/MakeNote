10ae31f6101325669091a7205b62b957cd238ca1
swati-93 <swati.1993cs@gmail.com>
2017-06-07 00:37:03 +0530

    initial commit

---
+++ app/src/main/java/makenote/swakez/github/io/MainActivity.java
@@ -0,0 +1,17 @@
+package makenote.swakez.github.io;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DBOpenHelper helper = new DBOpenHelper(this);
+        SQLiteDatabase database = helper.getWritableDatabase();
+    }
+}
---
+++ app/src/main/java/makenote/swakez/github/io/NotesProvider.java
@@ -0,0 +1,8 @@
+package makenote.swakez.github.io;
+
+/**
+ * Created by swati on 07/06/2017.
+ */
+
+public class NotesProvider {
+}
