6c5830d8e80a9d68033979fbf59f1057e69c42b9
swati-93 <swati.1993cs@gmail.com>
2017-06-07 14:18:30 +0530

    Loader interface implemented

--- app/src/main/java/makenote/swakez/github/io/MainActivity.java
+++ app/src/main/java/makenote/swakez/github/io/MainActivity.java
@@ -1,15 +1,102 @@
 package makenote.swakez.github.io;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+implements LoaderManager.LoaderCallbacks<Cursor>
+{
+        CursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         }
+        insertNote("new note");
+
+        String[] from = {DBOpenHelper.NOTE_TEXT};
+        int[] to = {android.R.id.text1};
+        cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1, null, from, to, 0);
+
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT,noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI,
+                values);
+        Log.d("MainActivity", "inserted note" + noteUri.getLastPathSegment());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_create_sample:
+                insertSampleData();
+                break;
+            case R.id.action_delete_all:
+                deleteAllNotes();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteAllNotes() {
+        
+    }
+
+    private void insertSampleData() {
+        insertNote("Simple note");
+        insertNote("Multi-line\nnote");
+        insertNote("Verylong note with a lot of tesxt that exceeds the width if the screen");
+        restartLoader();
+    }
+
+    private void restartLoader() {
+        getLoaderManager().restartLoader(0,null,this);
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this, NotesProvider.CONTENT_URI,
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
+    }
 }
--- app/src/main/java/makenote/swakez/github/io/NotesProvider.java
+++ app/src/main/java/makenote/swakez/github/io/NotesProvider.java
@@ -42,8 +42,11 @@ public class NotesProvider extends ContentProvider{
 
     @Nullable
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
-        return null;
+    public Cursor query(Uri uri, String[] projection, String selection, String[]
+            selectionArgs, String sortOrder) {
+        return database.query(DBOpenHelper.TABLE_NOTES, DBOpenHelper.ALL_COLUMNS,
+                selection, null, null, null,
+                DBOpenHelper.NOTE_CREATED + " DESC");
     }
 
     @Nullable
@@ -55,16 +58,20 @@ public class NotesProvider extends ContentProvider{
     @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        long id = database.insert(DBOpenHelper.TABLE_NOTES,
+                null,contentValues);
+        return Uri.parse(BASE_PATH + "/" + id);
     }
 
     @Override
-    public int delete(Uri uri, String s, String[] strings) {
-        return 0;
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return database.delete(DBOpenHelper.TABLE_NOTES, selection, selectionArgs);
     }
 
     @Override
-    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
-        return 0;
+    public int update(Uri uri, ContentValues contentValues, String selection, String[]
+            selectionArgs) {
+        return database.update(DBOpenHelper.TABLE_NOTES,
+                contentValues, selection, selectionArgs);
     }
 }
