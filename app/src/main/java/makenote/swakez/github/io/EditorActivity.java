ef0181b8d9971ee52fb3bc456dd4c636f77aba6c
swati-93 <swati.1993cs@gmail.com>
2017-06-10 14:03:35 +0530

    editing note partially implemented

--- app/src/main/java/makenote/swakez/github/io/EditorActivity.java
+++ app/src/main/java/makenote/swakez/github/io/EditorActivity.java
@@ -2,9 +2,11 @@ package makenote.swakez.github.io;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -13,6 +15,8 @@ public class EditorActivity extends AppCompatActivity {
 
     private String action;
     private EditText editor;
+    private String noteFilter;
+    private String oldText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,17 @@ public class EditorActivity extends AppCompatActivity {
         if (uri == null) {
             action = Intent.ACTION_INSERT;
             setTitle(getString(R.string.new_note));
+        } else {
+            action = Intent.ACTION_EDIT;
+            noteFilter = DBOpenHelper.NOTE_ID + "/" + uri.getLastPathSegment();
+            Log.d("EditorActivity", noteFilter);
+
+            Cursor cursor = getContentResolver().query(uri,
+                    DBOpenHelper.ALL_COLUMNS, noteFilter, null, null);
+            cursor.moveToNext();
+            oldText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+            editor.setText(oldText);
+            editor.requestFocus();
         }
     }
 
@@ -51,6 +66,7 @@ public class EditorActivity extends AppCompatActivity {
 
     public void finishEditing() {
         String newNoteText = editor.getText().toString().trim();
+
         if (newNoteText.length() == 0) {
             setResult(RESULT_CANCELED);
         } else {
@@ -62,8 +78,7 @@ public class EditorActivity extends AppCompatActivity {
     private void insertNewNote(String newNoteText) {
         ContentValues values = new ContentValues();
         values.put(DBOpenHelper.NOTE_TEXT, newNoteText);
-        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI,
-                values);
+        getContentResolver().insert(NotesProvider.CONTENT_URI, values);
         setResult(RESULT_OK);
     }
 
